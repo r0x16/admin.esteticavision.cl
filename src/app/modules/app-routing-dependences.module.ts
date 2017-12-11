14b5df0a8684b5a25724d6cfc33cb91071adbb33
RibonKB <ribon22x@gmail.com>
2017-12-11 09:48:12 -0300

    Added CKEditor Dependence as Asset
    Se agrega la librería para editores WYSIWYG CKEditor que será utilizada en algunos módulos, como librería en los assets.
    Se desconoce como poder mejorar esta inserción poniendola como una dependencia NPM, será investigado.

--- src/app/modules/app-routing-dependences.module.ts
+++ src/app/modules/app-routing-dependences.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../modules/material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CKEditorModule } from 'ng2-ckeditor';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    CKEditorModule
   ],
   exports: [
     CommonModule,
     MaterialModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    CKEditorModule
   ]
 })
 export class AppRoutingDependencesModule { }
